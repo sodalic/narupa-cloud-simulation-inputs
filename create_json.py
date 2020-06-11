932fd41aa5b5feff70c183fa1f1ffa3ffcecb282
Jonathan Barnoud <jonathan@barnoud.net>
2020-06-11 17:47:30 +0100

    Mimick the json database

---
+++ create_json.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from pathlib import Path
+import json
+
+here = Path('.')
+all_xml = here.glob('*.xml')
+
+manifest = []
+for xml in all_xml:
+    name = xml.stem
+    content = {
+        'description': 'Lorem ipsum dolor sit amet.',
+        'simulation': str(xml),
+    }
+    for runner in ('ase', 'omm'):
+        json_path = xml.with_suffix(f'.{runner}.json')
+        content['runner'] = runner
+        content['name'] = f'{name}-{runner}'
+        manifest.append(json_path)
+        with open(json_path, 'w') as outfile:
+            json.dump(content, outfile)
+
+with open('manifest.txt', 'w') as outfile:
+    for path in manifest:
+        print(path, file=outfile)
+
